262dba6f505d0776a0b347786eedb83e56c2ae80
Pasi P <pjpakkala@gmail.com>
2023-01-28 21:15:09 +0000

    Spanish word list is added, spanish web pages and functions are added. Next: Need to show correct and user's wrong answer on web page to understand current issue of unnecessary mistakes showing up.

--- functions.py
+++ functions.py
@@ -81,6 +81,8 @@ def right_or_wrong(correct_word, user_answer):
 
 
 
+
+
 swe_words = ["vecka", "år", "idag", "imorgon", "igår",
              "kalender", "sekund", "timme", "minut", "klockan",
              "klocka", "en timme", "kan", "använda", "göra",
@@ -158,3 +160,98 @@ def translate_to_swedish_from_end(index=""):
         eng_word = eng_swe_words[index]
         swe_word = swe_words[index]
         return(eng_word, swe_word, index)
+
+
+
+
+
+spanish_words = ["hola", "buenas", "buenos días", "buenas tardes", "buenas noches",
+                 "chao / adios", "hasta luego", "hasta pronto", "hasta mañana", "nos vemos",
+                 "Que tengas un buen día", "por favor", "gracias", "muchas gracias", "de nada", 
+                 "con permiso", "disculpe / perdón", "me llamo", "Mi nombre es..",
+                 "Cúal es tu nombre?", "Cómo te llamas?", "mucho gusto", "igualmente",
+                 "quien?", "qué?", "dónde?", "cuando?", "por qué?", "cómo?", "cuánto cuesta?",
+                 "a qué hora?", "Cómo estás?", "Qué tal?", "Como te va?", "Como te vas?",
+                 "Qué pasa", "Estoy bien", "Estoy mal", "De dónde eres?", "Soy de..",
+                 "Lost Estados Unidos", "China", "Japón", "Alemania", "Francia",
+                 "Inglaterra", "Polonia", "una / o", "dos", "tres", "quatro", "cinco",
+                 "seis", "siete", "ocho", "nueve", "diez", "viente", "treinta",
+                 "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa",
+                 "cien", "la tienda", "Dónde está la tienda?", "Me gusta..", "Esta camisa",
+                 "Estos pantalones", "Este suéter", "Este abrigo", "Esta billetera",
+                 "Este bolso", "Estos zapatos", "Puedo probarmelo?", "Tienes esto en otro tamaño",
+                 "restaurante", "desayuno", "almuerzo", "comida", "cena",
+                 "My gustaria pedir", "Me gustaria comer", "le pidor por",
+                 "por favor me da", "cerveza", "vino tinto", "vino blanco", "agua",
+                 "jugo", "zumo", "té", "café", "sopa", "carne", "pescado", "pan",
+                 "queso", "helado", "chocolate", "Dónde está este restaurante?",
+                 "Puedes traerme la cuenta, por favor?", "Me trae la cuenta, por favor?",
+                 "tren", "avión", "bilete", "boleto", "Adónde va este autobús?",
+                 "el hotel", "el hostel", "la playa", "el supermercado",
+                 "el centro commercial", "sigue recto", "gira a la derecha",
+                 "gira a la izquierda", "A qué distancia está..", "Puedes ayudarme, por favor?",
+                 "Estoy perdido", "Necesito ayuda", "Me siento enfermo",
+                 "Dondé está el hospital?", "Necesito llamar a la policia",
+                 "Necesito llamar a la ambulancia",
+                ]
+
+
+spanish_eng_words = ["hello", "hi", "good morning", "good afternoon", "good night",
+                     "goodbye", "see you later", "see you soon", "see you tomorrow", "see you",
+                     "Have a great day", "please", "thank you", "thank you very much", "you're welcome", "excuse me", 
+                     "sorry", "My name is..", "My name is..", "What is your name?", "What is your name?",
+                     "Nice to meet you", "likewise", "who?", "what?", "where?", "when?",
+                     "why?", "how?", "how much?", "at what time?", "How are you?", "How are you?",
+                     "How's it going?", "How are you doing?", "What's up?", "I'm good",
+                     "I'm bad", "Where are you from?", "I'm from..", "The US", "China",
+                     "Japan", "Germany", "France", "England", "Poland", "1", "2", "3",
+                     "4", "5", "6", "7", "8", "9", "10", "20", "30", "40", "50", "60",
+                     "70", "80", "90", "100", "the store", "Where is the store?",
+                     "I like..", "This shirt", "These pants", "That sweater", "That coat",
+                     "That wallet", "That purse", "These shoes", "Can I try it on?",
+                     "Do you have this in another size?", "restaurant", "breakfast",
+                     "lunch", "lunch", "dinner", "I would like to order",
+                     "I would like to eat", "I ask you for", "please give me",
+                     "beer", "red wine", "white wine", "water", "juice", "juice", "tea",
+                     "coffee", "soup", "meat", "fish", "bread", "cheese", "ice cream",
+                     "chocolate", "Where is this restaurant?", "Can you bring me the bill, please?",
+                     "Can you bring me the bill, please?", "train", "airplane", "ticket",
+                     "ticket", "Where does this bus go?", "the hotel", "the hostel",
+                     "the beach", "the supermarket", "the shopping mall", "continue straight",
+                     "turn right", "turn left", "How far is..", "Can you help me, please?",
+                     "I'm lost", "I need help", "I feel sick", "Where is the hospital?",
+                     "I need to call the police", "I need to call the ambulance",
+                    ]
+
+
+def translate_to_spanish_from_start(index=""):
+
+    #Right now code does not react accordingly to reaching maximum index
+    
+    if index == "":
+        index = 0
+        #need to add a WIN option when all words are cleared
+        eng_word = spanish_eng_words[index]
+        es_word = spanish_words[index]
+        return(eng_word, es_word, index)
+    else: 
+        index = int(index)
+        index += 1
+        eng_word = spanish_eng_words[index]
+        es_word = spanish_words[index]
+        return(eng_word, es_word, index)
+
+
+
+def translate_to_spanish_from_end(index=""):
+    if index == "":
+        index = len(spanish_words) -1
+        eng_word = spanish_eng_words[index]
+        es_word = spanish_words[index]
+        return(eng_word, es_word, index)
+    else: 
+        index = int(index)
+        index -= 1
+        eng_word = spanish_eng_words[index]
+        es_word = spanish_words[index]
+        return(eng_word, es_word, index)
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from flask import Flask, redirect, render_template, request, session
 import os
 from functions import translate_to_slovak_from_start, translate_to_slovak_from_end, right_or_wrong
 from functions import translate_to_swedish_from_start, translate_to_swedish_from_end
+from functions import translate_to_spanish_from_start, translate_to_spanish_from_end
 
 
 #Tasks
-#Using database to track learning
+#Using database / sessions to track learning
 #Using database to re-ask mistakes
+#Show on screen correct answer and user's answer when there is a mistake
 #Using CSS make website nicer to look at 
 #Using CSS to make website fit on screens
 #Add stats on main page
 #Add more Slovak words
-#Add Spanish words
 #In future learning about JS and React etc and seeing how those can be used to benefit this project
+
 
 app = Flask(__name__)
 app.secret_key = os.environ['session_key']
@@ -105,7 +107,7 @@ def reverse_swe():
 
     index = form_data["index_number"]
 
-    words = translate_to_swedish_from_end(index)
+    words = spanish_words(index)
     
     return render_template("swe_words_reverse.html", 
                             word_to_translate = words[0], 
@@ -113,6 +115,51 @@ def reverse_swe():
                             index_follow_up = words[2], 
                             correct_or_wrong = right_wrong )
 
+
+
+
+@app.route("/spanish_words", methods=["POST"])
+def straight_es():
+    form_data = request.form
+    user_answer = form_data["user_answer"]
+    
+    correct_word = form_data["correct_answer"]
+    if correct_word == "":
+        right_wrong = ""
+    else:    
+        right_wrong = right_or_wrong(correct_word, user_answer)
+
+    index = form_data["index_number"]
+
+    words = translate_to_spanish_from_start(index)
+    
+    return render_template("es_words.html", 
+                            word_to_translate = words[0], 
+                            correct_answer = words[1],
+                            index_follow_up = words[2], 
+                            correct_or_wrong = right_wrong )
+
+    
+@app.route("/spanish_words_rev", methods=["POST"])
+def reverse_es():
+    form_data = request.form
+    user_answer = form_data["user_answer"]
+    
+    correct_word = form_data["correct_answer"]
+    if correct_word == "":
+        right_wrong = ""
+    else:    
+        right_wrong = right_or_wrong(correct_word, user_answer)
+
+    index = form_data["index_number"]
+
+    words = translate_to_spanish_from_end(index)
+    
+    return render_template("es_words_reverse.html", 
+                            word_to_translate = words[0], 
+                            correct_answer = words[1],
+                            index_follow_up = words[2], 
+                            correct_or_wrong = right_wrong )
 
 
 app.run(host="0.0.0.0", port=81)
